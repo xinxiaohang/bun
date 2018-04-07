02303f9d2d95f5109371af5c8897222ba8bbe508
xinxiaohang <1057273104@qq.com>
2018-04-07 22:38:39 +0800

    完善环境

---
+++ bun-sys-service/src/main/java/com/bun/xh/service/api/CommentFacadeImpl.java
@@ -0,0 +1,9 @@
+package com.bun.xh.service.api;
+
+import com.bun.xh.api.CommentFacade;
+
+/**
+ * Created by xin on 2018/4/7.
+ */
+public class CommentFacadeImpl implements CommentFacade {
+}
---
+++ bun-sys-service/src/main/java/com/bun/xh/service/api/NewsFacadeImpl.java
@@ -0,0 +1,9 @@
+package com.bun.xh.service.api;
+
+import com.bun.xh.api.NewsFacade;
+
+/**
+ * Created by xin on 2018/4/7.
+ */
+public class NewsFacadeImpl implements NewsFacade {
+}
