55820308e74f8f94ce8e4103d078734f2643d390
xinxiaohang <1057273104@qq.com>
2018-04-07 23:09:07 +0800

    增加类

---
+++ bun-sys-api/src/main/java/com/bun/xh/exception/BunServiceException.java
@@ -0,0 +1,7 @@
+package com.bun.xh.exception;
+
+/**
+ * Created by xin on 2018/4/7.
+ */
+public class BunServiceException extends Exception{
+}
---
+++ bun-sys-repository/src/main/java/com/bun/xh/repository/db/service/NewLogService.java
@@ -0,0 +1,7 @@
+package com.bun.xh.repository.db.service;
+
+/**
+ * Created by xin on 2018/4/7.
+ */
+public class NewLogService {
+}
